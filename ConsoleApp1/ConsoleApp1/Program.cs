41579b71184745c7fb48ba8d1a70690e03af9320
NatiaBz <nbzishvili1@gmail.com>
2024-10-24 21:38:48 +0400

    task1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ასაკი
+            int age = 39;
+            // სიმაღლე
+            float height = 1.72f; // მეტრებში
+            // წონა
+            float weight = 70f; // კილოგრამში
+            // სახელი
+            string name = "NATIA";
+            // დასაქმების სტატუსი
+            bool isemployed = true;
+            // საკონტაქტო ინფორმაცია
+            string email = "nbzishvili1@gmail.com";
+            string mobile = "598112055";
+
+            // ბეჭდვა
+            Console.WriteLine("my infomation:");
+            Console.WriteLine($"name: {name}");
+            Console.WriteLine($"age: {age} Year");
+            Console.WriteLine($"height: {height} m");
+            Console.WriteLine($"weight: {weight} kg");
+            Console.WriteLine($"email: {email}");
+            Console.WriteLine($"mobile: {mobile}");
+            Console.WriteLine($"isemployed:{isemployed}");
+
+            // პროგრამის დასრულება
+            Console.ReadLine();
+        }
+    }
+}
+
+
