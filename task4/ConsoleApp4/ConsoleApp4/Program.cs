ae6b2ef4b501fe30e9822de632cc87d413c1679c
NatiaBz <nbzishvili1@gmail.com>
2024-11-07 21:41:40 +0400

    forLoop

---
+++ task4/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Input a number
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // for loop printing the multiplication table
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} x {i} = {number * i}");
+        }
+    }
+}
+
