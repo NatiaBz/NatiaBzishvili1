9a33f5c3b7117e93d1b7219ad1d9ad06c828808d
NatiaBz <nbzishvili1@gmail.com>
2024-11-07 21:36:00 +0400

    while

---
+++ task4/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        int number;
+
+        // while Cycle for adding positive numbers
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                break; // Stopping the loop if the number is negative
+            }
+
+            sum += number; // Adding the number to the total sum
+        }
+
+        // Printing the sum
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
+
