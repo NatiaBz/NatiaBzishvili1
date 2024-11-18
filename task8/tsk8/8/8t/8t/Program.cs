47d03967ad90661166e89978728c8543b1982e80
NatiaBz <nbzishvili1@gmail.com>
2024-11-18 19:25:05 +0400

    me8

---
+++ task8/tsk8/8/8t/8t/Program.cs
@@ -0,0 +1,35 @@
+﻿//
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    // konstruqtori, romelic parametrad igebs name da age
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // SevqmnaT Person klasis obieqti konstruqtoris saSualebit
+        Person person = new Person("Natia", 39);
+
+        // vaCveoT obieqtis monacemebi
+        Console.WriteLine($"Name: {person.Name}");
+        Console.WriteLine($"Age: {person.Age}");
+
+        // SevcvaloT obieqtis monacemebi
+        person.Name = "Vakho";
+        person.Age = 40;
+
+        // vachvenot ganaxlebulil monacmeebi
+        Console.WriteLine($"Updated Name: {person.Name}");
+        Console.WriteLine($"Updated Age: {person.Age}");
+    }
+}
